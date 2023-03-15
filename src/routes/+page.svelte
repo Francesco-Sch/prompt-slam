6ba9e782f9106e75475ad7e9a3a19ae732a3c5e0
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-03-15 15:55:56 +0100

    Add Redaction font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,1 @@
-<h1 class="text-3xl font-bold underline">
-    Hello World
-</h1>
+<h1 class="text-3xl underline">Hello World</h1>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,15 @@ module.exports = {
 				DEFAULT: '#388697'
 			}
 		},
+		fontFamily: {
+			redaction: ['Redaction', 'system-ui', 'serif'],
+			'redaction-10': ['"Redaction 10"', 'system-ui', 'serif'],
+			'redaction-20': ['"Redaction 20"', 'system-ui', 'serif'],
+			'redaction-35': ['"Redaction 35"', 'system-ui', 'serif'],
+			'redaction-50': ['"Redaction 50"', 'system-ui', 'serif'],
+			'redaction-70': ['"Redaction 70"', 'system-ui', 'serif'],
+			'redaction-100': ['"Redaction 100"', 'system-ui', 'serif']
+		},
 		extend: {}
 	},
 	plugins: []
