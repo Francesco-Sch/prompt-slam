85e16aa701a3b06d2d3fb1b9a81c9d9be1c5c83f
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-03-15 15:26:02 +0100

    Add temporary prompt battle colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		colors: {
+			'prompt-orange': {
+				DEFAULT: '#FA8334'
+			},
+			'prompt-blue': {
+				DEFAULT: '#388697'
+			}
+		},
 		extend: {}
 	},
 	plugins: []
