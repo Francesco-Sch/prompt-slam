b24388aeaf161b7f486e640002fc45ffb8efa7eb
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-03-17 16:15:36 +0100

    Change underline of user name input depending on theme value

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { theme } from '$lib/store';
 	import ThemeSwitch from '$lib/components/ThemeSwitch.svelte';
 	import ActionBar from '$lib/layout/ActionBar.svelte';
 </script>
@@ -9,7 +10,11 @@
 		<input
 			name="user-name"
 			type="text"
-			class="mt-5 w-full border-0 border-b border-slate-900 font-redaction text-8xl focus:border-b-prompt-orange focus:outline-none focus:ring-0"
+			class="mt-5 w-full border-0 border-b border-slate-900 font-redaction text-8xl focus:outline-none focus:ring-0
+			{$theme === 'orange'
+				? 'focus:border-b-prompt-orange'
+				: 'focus:border-b-prompt-blue'}
+			"
 		/>
 	</form>
 
