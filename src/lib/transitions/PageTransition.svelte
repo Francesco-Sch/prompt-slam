d9b19b458b534e7be75322cc87e95bb7800656ee
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-03-17 17:51:39 +0100

    Add transition between route changes

---
+++ src/lib/transitions/PageTransition.svelte
@@ -0,0 +1,17 @@
+<!-- 
+    Thanks to Evan Winter for this solution: 
+    https://github.com/evanwinter/sveltekit-page-transitions
+ -->
+<script>
+	import { fly } from 'svelte/transition';
+	export let pathname = '';
+</script>
+
+{#key pathname}
+	<div
+		in:fly={{ y: 10, duration: 450, delay: 500 }}
+		out:fly={{ y: -5, duration: 450 }}
+	>
+		<slot />
+	</div>
+{/key}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+	import type { LayoutData } from './$types';
 	import Header from '$lib/layout/Header.svelte';
+	import PageTransition from '$lib/transitions/PageTransition.svelte';
 	import '../styles/app.css';
+
+	export let data: LayoutData;
 </script>
 
 <Header />
 
-<main class="h-screen">
-	<slot />
-</main>
+<PageTransition pathname={data.pathname}>
+	<main class="h-screen">
+		<slot />
+	</main>
+</PageTransition>
