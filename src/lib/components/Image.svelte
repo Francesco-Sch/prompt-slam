bbfa981fdc7a5a3e0e62cf74bbc02d689f98367f
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-03-23 15:12:09 +0100

    Add download button without working download

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
+	import { IconDownload } from '@tabler/icons-svelte';
 	export let src: string;
 	export let alt: string;
 
 	export let clientHeight;
 </script>
 
-<div class="h-max" bind:clientHeight>
+<div class="relative h-max" bind:clientHeight>
+	<!-- Download Button -->
+	<a
+		href={src}
+		download
+		class="absolute top-4 right-4 rounded-lg bg-slate-50 p-2 transition-colors hover:bg-slate-200"
+	>
+		<IconDownload size={24} stroke={2} />
+	</a>
+
 	<img {src} {alt} />
 </div>
