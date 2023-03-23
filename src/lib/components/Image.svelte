cf923265ba4f5776e6ee8fe359f28ff45f8a5bb9
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-03-23 15:24:04 +0100

    Let download button appear an hover over image

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -6,15 +6,16 @@
 	export let clientHeight;
 </script>
 
-<div class="relative h-max" bind:clientHeight>
+<div class="group relative h-max" bind:clientHeight>
 	<!-- Download Button -->
 	<a
 		href={src}
 		download
-		class="absolute top-4 right-4 rounded-lg bg-slate-50 p-2 transition-colors hover:bg-slate-200"
+		class="absolute top-4 right-4 rounded-lg bg-white p-2 opacity-0 transition duration-200 hover:bg-slate-200 group-hover:opacity-100"
 	>
 		<IconDownload size={24} stroke={2} />
 	</a>
 
+	<!-- Image -->
 	<img {src} {alt} />
 </div>
