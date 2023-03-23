0a225b0670b72ffb7256a4639802c4af522ad93a
Francesco Scheffczyk <63101002+Francesco-Sch@users.noreply.github.com>
2023-03-23 13:06:06 +0100

    Control the size of the Result component better (#12)
    * Add max-width values to Tailwind depending an Viewport Width
    
    * Makes padding of Result component responsive
    
    * Compute height of text container depending on height of image
    
    * Change textContainer height when window was resized
    
    * Makes typography in Result component more responsive
    
    ---------
    
    Co-authored-by: Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>

--- src/lib/components/Result.svelte
+++ src/lib/components/Result.svelte
@@ -4,21 +4,47 @@
 	export let image: string;
 	export let prompt: string;
 	export let user: string | undefined = undefined;
+
+	// Compute height of prompt container.
+	// This makes sure, that the text on the right side is never bigger than
+	// the image on the left
+	let imageElement: HTMLImageElement;
+	let textContainerHeight = 0;
+	let windowWidth: any;
+
+	function setTextContainerHeight() {
+		textContainerHeight = imageElement.offsetHeight * 0.8;
+	}
+
+	$: if (windowWidth) {
+		setTextContainerHeight();
+		console.log('windowWidth changed.');
+	}
 </script>
 
+<svelte:window bind:innerWidth={windowWidth} />
+
 <div
-	class="grid auto-rows-auto grid-cols-2 gap-10 rounded-xl p-8"
+	class="grid max-h-screen-60 max-w-screen-80 grid-cols-2 gap-10 rounded-xl p-4 md:p-6 lg:p-8"
 	class:bg-prompt-orange={$theme === 'orange' && $colorizedBackground === false}
 	class:bg-prompt-blue={$theme === 'blue' && $colorizedBackground === false}
 	class:bg-slate-900={$colorizedBackground === true}
 	class:text-white={$colorizedBackground === true}
 >
-	<img src={image} />
+	<img
+		src={image}
+		alt="The generated result"
+		on:load={setTextContainerHeight}
+		bind:this={imageElement}
+	/>
 
-	<!-- <div class="mx-6 h-full w-px bg-slate-900" /> -->
-
-	<div class="flex flex-col">
-		<p class="font-redaction text-5xl">{prompt}</p>
-		<p class="mt-auto">by {user}</p>
+	<div class="flex h-full flex-col">
+		<p
+			class="overflow-y-auto font-redaction sm:mb-2 md:mb-5 md:text-2xl lg:text-5xl"
+			style="height: {textContainerHeight}px"
+		>
+			{prompt}
+		</p>
+		<p class="mt-auto text-xs md:text-base">by {user}</p>
 	</div>
 </div>
