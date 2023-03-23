2a77310dc9252d5f416f4e62d0531d432c949942
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-03-23 13:58:30 +0100

    Wrap image in Image component and simplify computation of textContainerHeight

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let src: string;
+	export let alt: string;
+
+	export let clientHeight;
+</script>
+
+<div class="h-max" bind:clientHeight>
+	<img {src} {alt} />
+</div>
