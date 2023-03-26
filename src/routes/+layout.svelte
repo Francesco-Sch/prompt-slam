169152a04a84e71423fe265d72f14a3340367128
Francesco Scheffczyk <francesco.scheffczyk@gmail.comx>
2023-03-26 17:44:27 +0200

    Add OG Image and Twitter Image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,48 @@
 <script lang="ts">
 	import type { LayoutData } from './$types';
+	import { page } from '$app/stores';
 	import { theme, colorizedBackground } from '$lib/store';
+	import { MetaTags } from 'svelte-meta-tags';
+
 	import Header from '$lib/layout/Header.svelte';
 	import PageTransition from '$lib/transitions/PageTransition.svelte';
 	import '../styles/app.css';
 
 	export let data: LayoutData;
 </script>
+
+<MetaTags
+	title="Prompt Battle"
+	description="Battle each other in an amazing prompt battle using the power of DALL-E."
+	openGraph={{
+		url: $page.url.href,
+		title: 'Prompt Battle',
+		description:
+			'Battle each other in an amazing prompt battle using the power of DALL-E.',
+		images: [
+			{
+				url: `${$page.url.origin}/og-image.png`,
+				width: 1200,
+				height: 630,
+				alt: 'Prompt Battle'
+			}
+		],
+		site_name: 'Prompt Battle'
+	}}
+	twitter={{
+		handle: '@handle',
+		site: '@site',
+		cardType: 'summary_large_image',
+		title: 'Prompt Battle',
+		description:
+			'Battle each other in an amazing prompt battle using the power of DALL-E.',
+		image: `${$page.url.origin}/twitter-image.png`,
+		imageAlt: 'Prompt Battle'
+	}}
+	facebook={{
+		appId: '1234567890'
+	}}
+/>
 
 <Header />
 
